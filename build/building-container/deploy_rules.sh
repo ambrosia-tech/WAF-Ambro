941f15932f9e337332d8711c7808bc7069c617ae
terino-delo <terino.delo@proton.me>
2023-07-26 14:34:36 -0400

    Finxing s3 syn of rules to support creds parameters

--- build/building-container/deploy_rules.sh
+++ build/building-container/deploy_rules.sh
@@ -5,6 +5,8 @@ TF_SCRIPT_DIR=$(dirname "$TF_SCRIPT_PATH")
 DEPLOY_TYPE=prod
 S3_BUCKET_ROOT=default
 RULES_LOCAL=rules
+CREDID=credid
+CREDD=credd
 
 if [[ -z $1 ]]; then
     echo "missing deploy type: prod|stag"
@@ -20,14 +22,23 @@ else
     S3_BUCKET_ROOT=$2
 fi
 
-mkdir -p $TF_SCRIPT_DIR/$RULES_LOCAL
-data=`echo QUtJQVlDNTZUNkdMUVVDNFdVUk46elVPU1dMNStvVm93K0ZoeHcrT1pVUkFPVXRveDR3dkhTeEZ2djN0bgo= | base64 -d`
-datain=(${data//:/ })
-dataid=${datain[0]}
-dataq=${datain[1]}
+if [[ -z $3 ]]; then
+    echo "missing s3 cred id : cred id"
+    exit
+else
+    CREDID=$3
+fi
 
+if [[ -z $4 ]]; then
+    echo "missing s3 cred id : cred id"
+    exit
+else
+    CREDD=$4
+fi
+
+mkdir -p $TF_SCRIPT_DIR/$RULES_LOCAL
 id=`echo QVdTX0FDQ0VTU19LRVlfSUQK | base64 -d`
 d=`echo QVdTX1NFQ1JFVF9BQ0NFU1NfS0VZCg== | base64 -d`
 
 S3_BUCKET_DIR=$S3_BUCKET_ROOT/rules/$DEPLOY_TYPE/rules
-export $id=$dataid; export $d=$dataq; aws s3 cp s3://$S3_BUCKET_DIR $TF_SCRIPT_DIR/$RULES_LOCAL --recursive
+export $id=$CREDID; export $d=$CREDD; aws s3 cp s3://$S3_BUCKET_DIR $TF_SCRIPT_DIR/$RULES_LOCAL --recursive
